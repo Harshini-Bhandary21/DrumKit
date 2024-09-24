fa2084fc027af8b00140287c039e19ff73253b80
Harshini-Bhandary21 <harshinibhandary21@gmail.com>
2024-09-24 18:44:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var innerHtmlText= this.innerHTML;
+        makeSound(innerHtmlText);
+        addAnimation(innerHtmlText)
+    });
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    addAnimation(event.key)
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio('tom-1.mp3');
+            audio.play();
+        break;
+
+        case "a":
+            var audio = new Audio('tom-2.mp3');
+            audio.play();
+        break;
+
+        case "s":
+            var audio = new Audio('tom-3.mp3');
+            audio.play();
+        break;
+
+        case "d":
+            var audio = new Audio('tom-4.mp3');
+            audio.play();
+        break;
+
+        case "j":
+            var audio = new Audio('snare.mp3');
+            audio.play();
+        break;
+
+        case "k":
+            var audio = new Audio('crash.mp3');
+            audio.play();
+        break;
+
+        case "l":
+            var audio = new Audio('kick-bass.mp3');
+            audio.play();
+        break;
+        default:
+    };
+}
+
+function addAnimation(currentKey){
+    var currenButton = document.querySelector("." + currentKey);
+    currenButton.classList.add("pressed");
+
+    setTimeout(function(){
+        currenButton.classList.remove("pressed");
+    }, 100);
+
+
+}
+
+
+
